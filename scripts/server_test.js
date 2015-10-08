9fa78c0505a580d208a660fc1df01a7be12b1d72
code-11 <brendan.ritter@yahoo.com>
2015-10-08 11:20:37 -0400

    Updated server test

--- scripts/server_test.js
+++ scripts/server_test.js
@@ -5,13 +5,37 @@ var fs = require('fs');
 
 var url = 'mongodb://localhost:27017/test';
 
-
 var server =require('./main');
 
+
+var getAllTestLocations = function (db, callback){
+	var all_test_locations=db.collection('locations').find(
+		{"name":"SERVERTEST"}
+	);
+	all_test_locations.toArray(function(err,docs){
+		assert.equal(err,null);
+		if (callback!=null){
+			callback(db,docs);
+		}
+	});
+}
+
+var removeTestLocations =function(db){
+	var removed=db.collection('locations').remove(
+		{"name":"SERVERTEST"},
+		function(err,results){
+			db.close();
+	});
+}
+
 var writeTest=function(db){
-	server.updateLocation(db,10,15, function(db){
-		server.findDocuments(db,function(db,doc){
-			console.log(doc.lat==10 && doc.lon==15 ? true : false);
+	server.updateLocation(db,10,15,"SERVERTEST", function(db){
+		getAllTestLocations(db,function(db,docs){
+			console.log(docs[0]);
+			assert.equal(docs.length,1);
+			assert.equal(docs[0].lat,10);
+			assert.equal(docs[0].lon,15);
+			removeTestLocations(db);
 		});
 	});
 }
