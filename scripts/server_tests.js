8fcdbf779a574af5836a3ac7cd0337fd4c9eac82
code-11 <brendan.ritter@yahoo.com>
2015-12-06 13:36:01 -0500

    Added ping test to server tests

--- scripts/server_tests.js
+++ scripts/server_tests.js
@@ -1,4 +1,30 @@
 var assert = require('assert');
 // var jquery = require('jquery');
 
-console.log("New file was ran");
+// Can we make a get request to the server, ie is it up?
+function ping_test(){
+		options={
+		url:"https://foodinator.herokuapp.com/",
+		method: "GET"
+	};
+
+	req = http.request(options, function(res) {
+		res.on('data', function (chunk) {
+			console.log('Received data: ' + chunk);
+		});
+		res.on('end', function() {
+			console.log('Finished request sucessfully');
+		});
+	});
+	req.on('error', function(e) {
+		//There was some sort of error, throw the error and fail the test
+		throw e.message;
+	});
+	req.end();
+}
+
+console.log("Running Tests:");
+console.log("--------------");
+ping_test();
+console.log("--------------");
+console.log("Ran All Tests");
